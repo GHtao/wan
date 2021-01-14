e066ece999b11c0d5cfd0e923c3044531e5959cb
gaotao <gaotao@tocel.com>
2021-01-14 10:39:50 +0800

    recycler view吸顶

--- wan_gt/app/src/main/java/com/gt/wan_gt/test/TestFragment.kt
+++ wan_gt/app/src/main/java/com/gt/wan_gt/test/TestFragment.kt
@@ -1,26 +1,13 @@
 package com.gt.wan_gt.test
 
-import android.animation.ObjectAnimator
-import android.animation.ValueAnimator
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.view.animation.LinearInterpolator
-import androidx.activity.result.ActivityResultCallback
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.gt.core.base.BaseFragment
 import com.gt.wan_gt.R
+import com.gt.wan_gt.views.StickDecoration
 import kotlinx.android.synthetic.main.fragment_test.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 /**
  * time 2020/10/20 0020
@@ -38,17 +25,18 @@ class TestFragment : BaseFragment<TestFragmentVM>() {
 //
 //        }
 //        activityResult.launch(Intent(requireContext(),TestFragment::class.java))
-        test_rv.addItemDecoration(DividerItemDecoration(requireContext(),DividerItemDecoration.VERTICAL))
+//        test_rv.addItemDecoration(DividerItemDecoration(requireContext(),DividerItemDecoration.VERTICAL))
+
         val arrayList = ArrayList<TestBean>()
         for(i in 1 ..19){
             val testBean = TestBean().apply {
-                if(i % 10 ==0){
-                    isAd = true
-                    image = R.mipmap.icon_wan_android
-                }else{
+//                if(i % 10 ==0){
+//                    isAd = true
+//                    image = R.mipmap.icon_wan_android
+//                }else{
+//                }
                     isAd = false
-                    title = "position-$i"
-                }
+                    title = "$i position-$i"
             }
             arrayList.add(testBean)
         }
@@ -56,6 +44,9 @@ class TestFragment : BaseFragment<TestFragmentVM>() {
         adapter.data = arrayList
         test_rv.adapter = adapter
         test_rv.layoutManager = LinearLayoutManager(requireContext())
+        test_rv.addItemDecoration(StickDecoration(requireContext()) {
+                arrayList[it].title.substring(0, 1)
+        })
         adapter.notifyDataSetChanged()
     }
 
